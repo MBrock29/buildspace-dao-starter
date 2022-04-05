d6e0a4dcb3a5dc9a814757e4c7bda0df3c4c4a24
MBrock29 <55901037+MBrock29@users.noreply.github.com>
2022-04-05 15:15:23 +0100

    Minting NFT + member section

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,21 @@
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+const editionDrop = sdk.getEditionDrop(
+  "0xbE33C8Ef2b25E6E1Cd8358AAE072DF884D7c9A8f"
+);
+
+(async () => {
+  try {
+    await editionDrop.createBatch([
+      {
+        name: "Leaf Village Headband",
+        description: "This NFT will give you access to NarutoDAO!",
+        image: readFileSync("scripts/assets/MikesDAO.JPG"),
+      },
+    ]);
+    console.log("✅ Successfully created a new NFT in the drop!");
+  } catch (error) {
+    console.error("failed to create the new NFT", error);
+  }
+})();
